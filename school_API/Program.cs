fb737343a43b9606db3ecb1eb711d966ad8df072
Kaio Soares <kaioantoniosoares@gmail.com>
2022-10-07 20:44:08 -0300

    Configuring an application

--- school_API/Program.cs
+++ school_API/Program.cs
@@ -3,7 +3,22 @@ using school_API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Allow CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
+
+//Allow CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins("http://localhost").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        builder.SetIsOriginAllowed(origin => true);
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +41,9 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+//Allow CORS
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
