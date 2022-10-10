9e0d126c377c8ce54f7eb8a22d53a71a6bc80709
Kaio Soares <kaioantoniosoares@gmail.com>
2022-10-10 16:16:40 -0300

    Displaying database data

--- school/src/components/CrudAluno/CrudAluno.js
+++ school/src/components/CrudAluno/CrudAluno.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './CrudAluno.css'
 import Main from '../template/Main'
 
 const title = "Cadastro de Alunos"
 
-const Alunos = [
+const urlAPI = "https://localhost:7008/api/aluno"
+const initialState = {
+    aluno: { id: 0, ra: '', nome: '', codCurso: 0 },
+    lista: []
+}
+
+/*const Alunos = [
     { 'id': 1, 'ra': 11111, 'nome': 'André', 'codCurso': 19 },
     { 'id': 2, 'ra': 22222, 'nome': 'Amanda', 'codCurso': 28 },
     { 'id': 3, 'ra': 33333, 'nome': 'Pedro', 'codCurso': 39 },
     { 'id': 4, 'ra': 44444, 'nome': 'Alice', 'codCurso': 59 },
-]
+]*/
 
 export default class CrudAluno extends Component {
+
+    state = { ...initialState }
+
+    componentDidMount() {
+        axios(urlAPI).then(resp => {
+            this.setState({ lista: resp.data })
+        })
+    }
 
     renderTable() {
         return (
@@ -25,7 +40,7 @@ export default class CrudAluno extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {Alunos.map(
+                        {this.state.lista.map(
                             (aluno) =>
 
                                 <tr key={aluno.id}>
