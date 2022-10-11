c424562fd88044c0b4e72f728395aa6b0ae4259d
Kaio Soares <kaioantoniosoares@gmail.com>
2022-10-10 22:02:09 -0300

    Controlled Components

--- school/src/components/CrudAluno/CrudAluno.js
+++ school/src/components/CrudAluno/CrudAluno.js
@@ -28,6 +28,107 @@ export default class CrudAluno extends Component {
         })
     }
 
+    limpar() {
+        this.setState({ aluno: initialState.aluno })
+    }
+
+    salvar() {
+        const aluno = this.state.aluno
+        aluno.codCurso = Number(aluno.codCurso)
+        const metodo = aluno.id ? 'put' : 'post'
+        const url = aluno.id ? `${urlAPI}/${aluno.id}` : urlAPI
+
+        axios[metodo](url, aluno)
+            .then(resp => {
+                const lista = this.getListaAtualizada(resp.data)
+                this.setState({ aluno: initialState.aluno, lista })
+            })
+    }
+
+    getListaAtualizada(aluno, add = true) {
+        const lista = this.state.lista.filter(a => a.id !== aluno.id)
+        if (add) lista.unshift(aluno)
+        return lista
+    }
+
+    atualizaCampo(event) {
+        //clonar usuário a partir do state, para não alterar o state diretamente
+        const aluno = { ...this.state.aluno };
+        //usar o atributo NAME do input para identificar o campo a ser atualizado
+        aluno[event.target.name] = event.target.value;
+        //atualizar o state
+        this.setState({ aluno });
+    }
+
+    carregar(aluno) {
+        this.setState({ aluno })
+    }
+
+    remover(aluno) {
+        const url = urlAPI + "/" + aluno.id;
+        if (window.confirm("Confirma remoção do aluno: " + aluno.ra)) {
+            console.log("entrou no confirm");
+
+            axios['delete'](url, aluno)
+                .then(resp => {
+                    const lista = this.getListaAtualizada(aluno, false)
+                    this.setState({ aluno: initialState.aluno, lista })
+                })
+        }
+
+    }
+
+    renderForm() {
+        return (
+            <div className="inclui-container">
+                <label> RA: </label>
+                <input
+                    type="text"
+                    id="ra"
+                    placeholder="RA do aluno"
+                    className="form-input"
+                    name="ra"
+
+                    value={this.state.aluno.ra}
+
+                    onChange={e => this.atualizaCampo(e)}
+                />
+                <label> Nome: </label>
+                <input
+                    type="text"
+                    id="nome"
+                    placeholder="Nome do aluno"
+                    className="form-input"
+                    name="nome"
+
+                    value={this.state.aluno.nome}
+
+                    onChange={e => this.atualizaCampo(e)}
+                />
+                <label> Código do Curso: </label>
+                <input
+                    type="number"
+                    id="codCurso"
+                    placeholder="0"
+
+                    className="form-input"
+                    name="codCurso"
+
+                    value={this.state.aluno.codCurso}
+                    onChange={e => this.atualizaCampo(e)}
+                />
+                <button className="btnSalvar"
+                    onClick={e => this.salvar(e)} >
+                    Salvar
+                </button>
+                <button className="btnCancelar"
+                    onClick={e => this.limpar(e)} >
+                    Cancelar
+                </button>
+            </div>
+        )
+    }
+
     renderTable() {
         return (
             <div className="listagem">
@@ -47,6 +148,16 @@ export default class CrudAluno extends Component {
                                     <td>{aluno.ra}</td>
                                     <td>{aluno.nome}</td>
                                     <td>{aluno.codCurso}</td>
+                                    <td>
+                                        <button onClick={() => this.carregar(aluno)} >
+                                            Altera
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => this.remover(aluno)} >
+                                            Remove
+                                        </button>
+                                    </td>
                                 </tr>
                         )}
                     </tbody>
@@ -58,6 +169,7 @@ export default class CrudAluno extends Component {
     render() {
         return (
             <Main title={title}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
